87b7940c6c0cd9777c28c6dd85625715e4a0da79
kleyzon <kleyzon@pop-os.localdomain>
2022-04-22 00:40:20 -0600

    ActuAL

---
+++ backend.js
@@ -0,0 +1,17 @@
+//este backend vive en servidor ecterno y no tiene acceso a pag web
+
+export const user = {
+fullname:"",
+  username: "",
+password: "",
+
+register: function(){
+  console.log("From backend register");
+},
+login() {},
+
+
+
+
+}
+
--- principal.js
+++ principal.js
@@ -1,16 +1,36 @@
-function datos() {
-  var usuario = document.getElementById("usuario").value;
-  var contrasena = document.getElementById("contrasena").value;
+import { user } from "./backend.js";
 
-  if ((usuario == " ") | (contrasena == " ")) {
-    let div = document.getElementById("error").value;
+export default {
+  registro() {
+    const fullmname = document.getElementById("fullname").value;
+    const usuario = document.getElementById("usuario").value;
+    const contrasena = document.getElementById("contrasena").value;
 
-    div.innerHTML = "<p>Faltan datos requeridos<p>";
-  } else {
-    // api.usuario = usuario;
-    // api.contrasena = contrasena;
+    if (usuario === "" || contrasena === "" || fullname === "") {
+      const error = document.getElementById("error");
 
-    // api.registro();
-  }
-}
+      error.innerHTML = "<p>Faltan datos requeridos<p>";
+    } else {
 
+      //TODO enviar datos de usuario al backend
+      //ejemplo: user.fullneme = fullname
+      user.register();
+    }
+  },
+  datos() {
+    console.log("inicia datos");
+    const usuario = document.getElementById("usuario").value;
+    const contrasena = document.getElementById("contrasena").value;
+    console.log(usuario, contrasena);
+
+    if ((usuario === "") | (contrasena === "")) {
+      const error = document.getElementById("error");
+
+      error.innerHTML = "<p>Faltan datos requeridos<p>";
+    } else {
+      // api.usuario = usuario;
+      // api.contrasena = contrasena;
+      // api.registro();
+    }
+  },
+};
