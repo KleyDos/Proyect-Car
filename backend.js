ff8d11206c40290df814277e531ea277cb5db45e
car <carlos.echc11@gmail.com>
2022-05-04 00:06:05 -0600

    register

--- backend.js
+++ backend.js
@@ -19,7 +19,39 @@ export const user = {
 
       return true;
     } catch (error) {
-      console.log("Error: ", error);
+      console.error("Error: ", error);
+      return false;
+    }
+  },
+
+  getInfo() {
+    try {
+      this.fullname = localStorage.getItem("fullname");
+      this.username = localStorage.getItem("username");
+
+      if (this.fullname === null || this.username === null)
+        throw "No hay datos en el localStorage";
+
+      return {
+        fullname: this.fullname,
+        username: this.username,
+      };
+    } catch (error) {
+      console.error("Error: ", error);
+      return false;
+    }
+  },
+
+  editUser(fname, us) {
+    try {
+      console.log("f", fname);
+      console.log("u", us);
+      localStorage.setItem("fullname", fname);
+      localStorage.setItem("username", us);
+
+      return true;
+    } catch (error) {
+      console.error("Error: ", error);
       return false;
     }
   },
