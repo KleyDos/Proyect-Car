e556854732ff2347c9cf3760d9911d1d801162f5
kleyzon <kleyzon@pop-os.localdomain>
2022-05-08 22:13:14 -0600

    semana 3

--- backend.js
+++ backend.js
@@ -7,7 +7,7 @@ export const user = {
 
   register: function () {
     try {
-      console.log("From backend register");
+      console.log("From backend register: ");
       console.log("fullname", this.fullname);
       console.log("username", this.username);
       console.log("password", this.password);
@@ -24,6 +24,37 @@ export const user = {
     }
   },
 
+  getInfo() {
+    try {
+      this.fullname = localStorage.getItem("fullname");
+      this.username = localStorage.getItem("username");
+
+      if (this.fullname === null || this.username === null)
+        throw "No hay datos en el localstorage";
+
+      return {
+        fullname: this.fullname,
+        username: this.username,
+      };
+    } catch (error) {
+      console.error("Error: ", error);
+      return false;
+    }
+  },
+
+  editUser(fname, us) {
+    try {
+      console.log("f", fname);
+      console.log("u", us);
+      localStorage.setItem("fullname", fname);
+      localStorage.setItem("username", us);
+
+      return true;
+    } catch (error) {
+      console.error("Error: ", error);
+      return false;
+    }
+  },
+
   login() {},
 };
-
--- principal.js
+++ principal.js
@@ -23,22 +23,38 @@ export default {
         error.innerHTML = "<p>Error al registrar el usuario<p>";
       }
 
-      console.log("respueta del bakend:, respuesta");
+      console.log("respueta del bakend: ", respuesta);
     }
   },
-  datos() {
-    console.log("inicia datos");
+  buscarPerfil() {
+    console.log("Buscando perfil...");
+    const respuesta = user.getInfo();
+    console.log("respuesta del backend: ", respuesta);
 
+    if (respuesta === false) window.location.href = "./login.html";
+
+    let nombreCompleto = document.getElementById("nombreCompleto");
+    let usuario = document.getElementById("usuario");
+
+    nombreCompleto.value = respuesta.fullname;
+    usuario.value = respuesta.username;
+  },
+  editar() {
+    console.log("Editando perfil...");
+    const nombreCompleto = document.getElementById("nombreCompleto").value;
     const usuario = document.getElementById("usuario").value;
-    const contrasena = document.getElementById("contrasena").value;
+    //user.fullname = nombreCompleto;
+    //user.username = usuario;
 
-    console.log(usuario, contrasena);
+    const respuesta = user.editUser(nombreCompleto, usuario);
+    if (!respuesta) {
+      const mensaje = document.getElementById("mensaje");
 
-    if ((usuario === "") | (contrasena === "")) {
-      const error = document.getElementById("error");
-
-      error.innerHTML = "<p>Faltan datos requeridos<p>";
+      mensaje.innerHTML = "<p>Ha ocurrido un error al editar<p>";
     } else {
+      const mensaje = document.getElementById("mensaje");
+
+      mensaje.innerHTML = "<p>Usuario guardado correctamente</p>";
     }
   },
 };
