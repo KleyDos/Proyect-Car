82442d2c6b97b5477096123c4c3aee30f6348074
kleyzon <kleyzon@pop-os.localdomain>
2022-08-22 20:45:25 -0600

    nini 22 08 22

--- backend.js
+++ backend.js
@@ -6,22 +6,7 @@ export const user = {
   password: "",
 
   register: function () {
-    try {
-      console.log("From backend register: ");
-      console.log("fullname", this.fullname);
-      console.log("username", this.username);
-      console.log("password", this.password);
-
-      //save data to local storage
-      localStorage.setItem("fullname", this.fullname);
-      localStorage.setItem("username", this.username);
-      localStorage.setItem("password", this.password);
-
-      return true;
-    } catch (error) {
-      console.log("Error", error);
-      return false;
-    }
+    
   },
 
   getInfo() {
